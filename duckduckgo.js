f627e751a4d9727ecf6945cc06d69fcf7f0e56c4
Juthawong Naisanguansee <juthawong@gmail.com>
2018-01-13 19:00:17 +0700

    Update duckduckgo.js

--- duckduckgo.js
+++ duckduckgo.js
@@ -1,11 +1,12 @@
-String.prototype.DuckDuckGo = function (keyword) {
+String.prototype.DuckDuckGo = function () {
     var amparerequest = new XMLHttpRequest();
     amparerequest.onload = function (e) {
         if (this.readyState == 4 && this.status == 200) {
-            return this.responseText;
+            return this.response;
         }
+        return false;
     }
-    amparerequest.open("GET", "http://api.duckduckgo.com/?q=" + keyword + "&format=json&pretty=1");
+    amparerequest.open("GET", "http://api.duckduckgo.com/?q=" + this + "&format=json&pretty=1");
     amparerequest.responseType = "json";
-    return false;
+    amparerequest.send();
 }
